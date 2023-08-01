68434f2356d2dc479ec55ea80d5a0595ebc715b7
akhfih <akhfih.dev@gmail.com>
2023-08-01 20:03:38 +0700

    7-4 managing static assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import vsharp from 'vite-plugin-vsharp';
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
   nitro: {
     prerender: {
       routes: ['/landing'],
     },
+  },
+  vite: {
+    plugins: [vsharp()],
   },
   modules: [
     '@nuxtjs/tailwindcss',
     '@vueuse/nuxt',
     '@nuxtjs/supabase',
     '@pinia/nuxt',
-  ]
-})
+  ],
+});
