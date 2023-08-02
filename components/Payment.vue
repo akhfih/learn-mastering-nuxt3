055fdb55105f5d9496b1a36e4d910797d0f67140
akhfih <akhfih.dev@gmail.com>
2023-08-02 10:35:13 +0700

    7-9 add paymentIntent endpoint

--- components/Payment.vue
+++ components/Payment.vue
@@ -1,31 +1,22 @@
 <template>
   <Modal @close="$emit('close')">
-    <div
-      class="bg-slate-200 p-8 rounded-xl w-full max-w-2xl"
-    >
-      <form>
-        <h2 class="font-bold text-xl text-center">
-          Buying {{ course.title }}
-        </h2>
-        <div
-          class="mt-8 text-base width bg-white py-6 px-8 rounded shadow-md"
+    <div class="bg-slate-200 p-8 rounded-xl w-full max-w-2xl">
+      <div
+        v-if="success"
+        class="flex flex-col justify-center items-center space-y-6"
+      >
+        <h2 class="text-xl font-bold">Thanks for buying the course!</h2>
+        <button
+          @click="login"
+          class="mt-4 w-full text-md text-black h-12 px-16 rounded focus:outline-none focus:shadow-outline flex items-center justify-center transition bg-blue-300 hover:bg-blue-200"
         >
-          <div
-            class="w-full flex justify-between items-center mb-8"<template>
-  <Modal @close="$emit('close')">
-    <div
-      class="bg-slate-200 p-8 rounded-xl w-full max-w-2xl"
-    >
-      <form>
-        <h2 class="font-bold text-xl text-center">
-          Buying {{ course.title }}
-        </h2>
-        <div
-          class="mt-8 text-base width bg-white py-6 px-8 rounded shadow-md"
-        >
-          <div
-            class="w-full flex justify-between items-center mb-8"
-          >
+          Login with Github to access
+        </button>
+      </div>
+      <form v-else @submit.prevent="handleSubmit">
+        <h2 class="font-bold text-xl text-center">Buying {{ course.title }}</h2>
+        <div class="mt-8 text-base width bg-white py-6 px-8 rounded shadow-md">
+          <div class="w-full flex justify-between items-center mb-8">
             <label class="font-bold"> Email </label>
             <input
               class="input ml-6 focus:outline-none text-left w-full"
@@ -43,9 +34,16 @@
         </div>
 
         <button
-          class="font-sans mt-4 w-full text-lg text-black h-12 px-16 rounded focus:outline-none focus:shadow-outline font-bold flex items-center justify-center transition bg-yellow-300 hover:bg-yellow-200 cursor-pointer"
+          class="font-sans mt-4 w-full text-lg text-black h-12 px-16 rounded focus:outline-none focus:shadow-outline font-bold flex items-center justify-center transition"
+          :class="
+            processingPayment || email === ''
+              ? 'bg-gray-300 cursor-not-allowed'
+              : 'bg-yellow-300 hover:bg-yellow-200 cursor-pointer'
+          "
+          :disabled="processingPayment || email === ''"
         >
-          <div>Pay $97</div>
+          <Loading v-if="processingPayment" class="h-5 w-5" />
+          <div v-else>Pay $97</div>
         </button>
       </form>
     </div>
@@ -94,30 +92,24 @@ const handleSubmit = async () => {
 
   try {
     // Create a PaymentIntent with the order amount and currency
-    const response = await $fetch(
-      '/api/stripe/paymentIntent',
-      {
-        method: 'POST',
-        body: {
-          email: email.value,
-        },
-      }
-    );
+    const response = await $fetch('/api/stripe/paymentIntent', {
+      method: 'POST',
+      body: {
+        email: email.value,
+      },
+    });
     secret = response;
   } catch (e) {
     console.log(e);
   }
 
   try {
-    const response = await stripe.value.confirmCardPayment(
-      secret,
-      {
-        payment_method: {
-          card: card.value,
-        },
-        receipt_email: email.value,
-      }
-    );
+    const response = await stripe.value.confirmCardPayment(secret, {
+      payment_method: {
+        card: card.value,
+      },
+      receipt_email: email.value,
+    });
 
     if (response.paymentIntent.status === 'succeeded') {
       success.value = true;
@@ -137,31 +129,4 @@ useHead({
     },
   ],
 });
-</script>
-
-          >
-            <label class="font-bold"> Email </label>
-            <input
-              class="input ml-6 focus:outline-none text-left w-full"
-              type="email"
-              autocomplete="email"
-              v-model="email"
-              placeholder="your@email.com"
-              required
-            />
-          </div>
-        </div>
-
-        <button
-          class="font-sans mt-4 w-full text-lg text-black h-12 px-16 rounded focus:outline-none focus:shadow-outline font-bold flex items-center justify-center transition bg-yellow-300 hover:bg-yellow-200 cursor-pointer"
-        >
-          <div>Pay $97</div>
-        </button>
-      </form>
-    </div>
-  </Modal>
-</template>
-
-<script setup>
-const course = await useCourse();
 </script>
